dab00889172621f1f13fdb8cc9dfc7aecb0ae77f
Dionisis Chatzimarmakis <dxatzimar@gmail.com>
2024-10-07 20:23:34 +0300

    Minor correction to keep updating every second to better follow requirements, in reall world I would set the update time internal 30 seconds though

--- StoiximanGR/StoiximanGR/Services/DataService/DataModels/SportsEvent.swift
+++ StoiximanGR/StoiximanGR/Services/DataService/DataModels/SportsEvent.swift
@@ -54,8 +54,8 @@ class SportsEvent: Codable, Identifiable, Equatable, Hashable, ObservableObject 
     }
    
     // Ok so updating so many data Events = Views every second is not good idea
-    // The reason that I kept it 2 second accuracy is that on real devices I didn't notice any performance issues, on simulator it has some minor scrolling gaps
-    // I would suggest to change the format that requirements defined: "00:00:00" and keep the remaining time max accuracy to minutes instead of seconds !
+    // The reason that I kept it 1 second accuracy is that on real devices I didn't notice any performance issues, on simulator it has some minor scrolling gaps
+    // I would suggest to change the format that requirements defined: "00:00:00" and keep the remaining time max accuracy to minutes instead of 1 second !
     // We could then set this variable to 30 seconds (realtimeSecondsToUpdateRemainingTime) to improve a lot the scrolling performance and
     // Also we should change the format of remaining time to show always "DD:HH:MM" = Days-Hour-Minutes -> example: "1d:22h:46m"
     // I did this already for the events that will start in more than 1 day -> format will be: "1d:22h:46m"
